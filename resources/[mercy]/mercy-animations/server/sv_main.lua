c553d980b72ed14493f8a1c81cf428d29cf253b5
Voporii <99855926+Voporii@users.noreply.github.com>
2023-06-28 12:50:05 -0600

    /e c command

--- resources/[mercy]/mercy-animations/server/sv_main.lua
+++ resources/[mercy]/mercy-animations/server/sv_main.lua
@@ -27,4 +27,8 @@ Citizen.CreateThread(function()
     CommandsModule.Add("e", "Play an animation.", {{Name="Animation", Help="Animation"}}, false, function(source, args)
         TriggerClientEvent('mercy-animations/client/play-animation', source, args[1])
     end)
+
+    CommandsModule.Add("e c", "Cancel the current animation.", {{Name="Animation", Help="Animation"}}, false, function(source, args)
+        TriggerClientEvent('mercy-animations/client/clear-animation', source)
+    end)
 end)
